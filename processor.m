4de13806c091c33b6ff35bcfb33ee064d7d41537
EdwardTourney <50584278+EdwardTourney@users.noreply.github.com>
2022-04-29 12:09:31 -0400

    fixed indexing
    fixed indexing

--- processor.m
+++ processor.m
@@ -10,12 +10,12 @@ cd jpg2
 file_Addr = dir('*.jpg');
 cd C:\Users\Myself\Documents\MATLAB\5660617
 
-% Shrink and Save Images
-for i = [1:112039]
-    flnm = join([file_Addr(i).folder,'\',file_Addr(i).name]);
-    art_img = imread(flnm);
-    small_img = imresize(art_img,[56,56]);
-    out_flnm = join([Output_folder,'\',file_Addr(i).name]);
-
-    imwrite(small_img,out_flnm);
-end
+% % Shrink and Save Images
+% for i = [1:112039]
+%     flnm = join([file_Addr(i).folder,'\',file_Addr(i).name]);
+%     art_img = imread(flnm);
+%     small_img = imresize(art_img,[56,56]);
+%     out_flnm = join([Output_folder,'\',file_Addr(i).name]);
+% 
+%     imwrite(small_img,out_flnm);
+% end
