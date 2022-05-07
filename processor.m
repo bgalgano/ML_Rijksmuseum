ebef9f124dab5e95796ebcb876e4a9b5fc7c8b9c
EdwardTourney <50584278+EdwardTourney@users.noreply.github.com>
2022-05-07 19:02:53 -0400

    Update processor.m

--- processor.m
+++ processor.m
@@ -3,19 +3,20 @@
 % Load Folders
 Main_folder = 'C:\Users\Myself\Documents\MATLAB\5660617';
 Input_folder = 'C:\Users\Myself\Documents\MATLAB\5660617\jpg2';
-Output_folder = 'C:\Users\Myself\Documents\MATLAB\5660617\out_img';
+Output_folder = 'C:\Users\Myself\Documents\MATLAB\5660617\out_img_bigger';
 
 % Load List of Filenames
 cd jpg2
 file_Addr = dir('*.jpg');
 cd C:\Users\Myself\Documents\MATLAB\5660617
 
-% % Shrink and Save Images
-% for i = [1:112039]
-%     flnm = join([file_Addr(i).folder,'\',file_Addr(i).name]);
-%     art_img = imread(flnm);
-%     small_img = imresize(art_img,[56,56]);
-%     out_flnm = join([Output_folder,'\',file_Addr(i).name]);
-% 
-%     imwrite(small_img,out_flnm);
-% end
+% Shrink and Save Images
+for i = [1:112039]
+    flnm = join([file_Addr(i).folder,'\',file_Addr(i).name]);
+    art_img = imread(flnm);
+    small_img = imresize(art_img,[224,224]);
+    out_flnm = join([Output_folder,'\',file_Addr(i).name]);
+
+    imwrite(small_img,out_flnm,'jpg');
+
+end
